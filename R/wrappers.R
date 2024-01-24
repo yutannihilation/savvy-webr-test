28bc53da03f12422f9846ce25a1835af7fa5894b
Hiroaki Yutani <yutani.ini@gmail.com>
2024-01-25 08:54:22 +0900

    Bring things back

--- R/wrappers.R
+++ R/wrappers.R
@@ -2,9 +2,303 @@
 #' @keywords internal
 NULL
 
+
+get_class_int <- function(x) {
+  .Call(get_class_int__impl, x)
+}
+
+
+get_names_int <- function(x) {
+  .Call(get_names_int__impl, x)
+}
+
+
+get_dim_int <- function(x) {
+  .Call(get_dim_int__impl, x)
+}
+
+
+get_attr_int <- function(x, attr) {
+  .Call(get_attr_int__impl, x, attr)
+}
+
+
+set_class_int <- function() {
+  .Call(set_class_int__impl)
+}
+
+
+set_names_int <- function() {
+  .Call(set_names_int__impl)
+}
+
+
+set_dim_int <- function() {
+  .Call(set_dim_int__impl)
+}
+
+
+set_attr_int <- function(attr, value) {
+  .Call(set_attr_int__impl, attr, value)
+}
+
+
+scalar_input_int <- function(x) {
+  invisible(.Call(scalar_input_int__impl, x))
+}
+
+
+scalar_input_usize <- function(x) {
+  invisible(.Call(scalar_input_usize__impl, x))
+}
+
+
+scalar_input_real <- function(x) {
+  invisible(.Call(scalar_input_real__impl, x))
+}
+
+
+scalar_input_logical <- function(x) {
+  invisible(.Call(scalar_input_logical__impl, x))
+}
+
+
+scalar_input_string <- function(x) {
+  invisible(.Call(scalar_input_string__impl, x))
+}
+
+
+scalar_output_int <- function() {
+  .Call(scalar_output_int__impl)
+}
+
+
+scalar_output_real <- function() {
+  .Call(scalar_output_real__impl)
+}
+
+
+scalar_output_logical <- function() {
+  .Call(scalar_output_logical__impl)
+}
+
+
+scalar_output_string <- function() {
+  .Call(scalar_output_string__impl)
+}
+
+
+sum_int <- function(x) {
+  .Call(sum_int__impl, x)
+}
+
+
+sum_real <- function(x) {
+  .Call(sum_real__impl, x)
+}
+
+
+rep_int_vec <- function(x) {
+  .Call(rep_int_vec__impl, x)
+}
+
+
+rep_int_slice <- function(x) {
+  .Call(rep_int_slice__impl, x)
+}
+
+
+rep_real_vec <- function(x) {
+  .Call(rep_real_vec__impl, x)
+}
+
+
+rep_real_slice <- function(x) {
+  .Call(rep_real_slice__impl, x)
+}
+
+
+rep_bool_vec <- function(x) {
+  .Call(rep_bool_vec__impl, x)
+}
+
+
+rep_bool_slice <- function(x) {
+  .Call(rep_bool_slice__impl, x)
+}
+
+
+rep_str_vec <- function(x) {
+  .Call(rep_str_vec__impl, x)
+}
+
+
+rep_str_slice <- function(x) {
+  .Call(rep_str_slice__impl, x)
+}
+
+
+safe_stop <- function() {
+  invisible(.Call(safe_stop__impl))
+}
+
+
+raise_error <- function() {
+  .Call(raise_error__impl)
+}
+
+
+new_int <- function(size) {
+  .Call(new_int__impl, size)
+}
+
+
+new_real <- function(size) {
+  .Call(new_real__impl, size)
+}
+
+
+new_bool <- function(size) {
+  .Call(new_bool__impl, size)
+}
+
+#' Convert Input To Upper-Case
+#'
+#' @param x A character vector.
+#' @returns A character vector with upper case version of the input.
 #' @export
-int_vec <- function() {
-  .Call(int_vec__impl)
+to_upper <- function(x) {
+  .Call(to_upper__impl, x)
+}
+
+#' Add suffix
+#'
+#' @param x A character vector.
+#' @param y A suffix.
+#' @returns A character vector with upper case version of the input.
+#' @export
+add_suffix <- function(x, y) {
+  .Call(add_suffix__impl, x, y)
+}
+
+#' Multiply Input By Two
+#'
+#' @param x An integer vector.
+#' @returns An integer vector with values multiplied by 2.
+#' @export
+times_two_int <- function(x) {
+  .Call(times_two_int__impl, x)
+}
+
+#' Multiply Input By Another Input
+#'
+#' @param x An integer vector.
+#' @param y An integer to multiply.
+#' @returns An integer vector with values multiplied by `y`.
+#' @export
+times_any_int <- function(x, y) {
+  .Call(times_any_int__impl, x, y)
+}
+
+#' Multiply Input By Two
+#'
+#' @param x A numeric vector.
+#' @returns A numeric vector with values multiplied by 2.
+#' @export
+times_two_numeric <- function(x) {
+  .Call(times_two_numeric__impl, x)
+}
+
+#' Multiply Input By Another Input
+#'
+#' @param x A real vector.
+#' @param y A real to multiply.
+#' @returns A real vector with values multiplied by `y`.
+#' @export
+times_any_numeric <- function(x, y) {
+  .Call(times_any_numeric__impl, x, y)
+}
+
+#' Flip Input
+#'
+#' @param x A logical vector.
+#' @returns A logical vector with filled values (`NA` is converted to `TRUE`).
+#' @export
+flip_logical <- function(x) {
+  .Call(flip_logical__impl, x)
+}
+
+#' Or operation
+#'
+#' @param x A logical vector.
+#' @param y A logical value.
+#' @returns A logical vector with filled values (`NA` is converted to `TRUE`).
+#' @export
+or_logical <- function(x, y) {
+  .Call(or_logical__impl, x, y)
+}
+
+#' Print the content of list
+#'
+#' @param x A list vector.
+#' @returns `NULL`
+#' @export
+print_list <- function(x) {
+  invisible(.Call(print_list__impl, x))
+}
+
+
+list_with_no_values <- function() {
+  .Call(list_with_no_values__impl)
+}
+
+
+list_with_no_names <- function() {
+  .Call(list_with_no_names__impl)
+}
+
+
+list_with_names_and_values <- function() {
+  .Call(list_with_names_and_values__impl)
+}
+
+
+my_integer <- function(x) {
+  invisible(.Call(my_integer__impl, x))
+}
+
+#' A person with a name
+#'
+#' @export
+Person <- function() {
+  e <- new.env(parent = emptyenv())
+  self <- .Call(Person_new__impl)
+
+  e$set_name <- Person_set_name(self)
+  e$name <- Person_name(self)
+  e$associated_function <- Person_associated_function(self)
+
+  class(e) <- "Person"
+  e
+}
+
+
+Person_set_name <- function(self) {
+  function(name) {
+    invisible(.Call(Person_set_name__impl, self, name))
+  }
+}
+
+Person_name <- function(self) {
+  function() {
+    .Call(Person_name__impl, self)
+  }
+}
+
+Person_associated_function <- function(self) {
+  function() {
+    .Call(Person_associated_function__impl)
+  }
 }
 
 
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,309 @@
 #![allow(unused_variables)]
-use savvy::savvy;
-use savvy::OwnedStringSexp;
 
+mod attributes;
+pub use attributes::*;
+
+mod convert_from_rust_types;
+pub use convert_from_rust_types::*;
+
+mod error_handling;
+pub use error_handling::*;
+
+mod try_from;
+pub use try_from::*;
+
+mod init_vectors;
+pub use init_vectors::*;
+
+use savvy::{r_print, savvy, OwnedListSexp};
+
+use savvy::{
+    IntegerSexp, ListSexp, LogicalSexp, OwnedIntegerSexp, OwnedLogicalSexp, OwnedRealSexp,
+    OwnedStringSexp, RealSexp, StringSexp, TypedSexp,
+};
+
+use savvy::sexp::na::NotAvailableValue;
+
+/// Convert Input To Upper-Case
+///
+/// @param x A character vector.
+/// @returns A character vector with upper case version of the input.
 /// @export
 #[savvy]
-fn int_vec() -> savvy::Result<savvy::Sexp> {
-    let out = OwnedStringSexp::new(3)?;
+fn to_upper(x: StringSexp) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedStringSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out.set_elt(i, <&str>::na())?;
+            continue;
+        }
+
+        let e_upper = e.to_uppercase();
+        out.set_elt(i, &e_upper)?;
+    }
+
     out.into()
+}
+
+/// Add suffix
+///
+/// @param x A character vector.
+/// @param y A suffix.
+/// @returns A character vector with upper case version of the input.
+/// @export
+#[savvy]
+fn add_suffix(x: StringSexp, y: &str) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedStringSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out.set_elt(i, <&str>::na())?;
+            continue;
+        }
+
+        out.set_elt(i, &format!("{e}_{y}"))?;
+    }
+
+    out.into()
+}
+
+/// Multiply Input By Two
+///
+/// @param x An integer vector.
+/// @returns An integer vector with values multiplied by 2.
+/// @export
+#[savvy]
+fn times_two_int(x: IntegerSexp) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedIntegerSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out[i] = i32::na();
+        } else {
+            out[i] = e * 2;
+        }
+    }
+
+    out.into()
+}
+
+/// Multiply Input By Another Input
+///
+/// @param x An integer vector.
+/// @param y An integer to multiply.
+/// @returns An integer vector with values multiplied by `y`.
+/// @export
+#[savvy]
+fn times_any_int(x: IntegerSexp, y: i32) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedIntegerSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out[i] = i32::na();
+        } else {
+            out[i] = e * y;
+        }
+    }
+
+    out.into()
+}
+
+/// Multiply Input By Two
+///
+/// @param x A numeric vector.
+/// @returns A numeric vector with values multiplied by 2.
+/// @export
+#[savvy]
+fn times_two_numeric(x: RealSexp) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedRealSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out[i] = f64::na();
+        } else {
+            out[i] = e * 2.0;
+        }
+    }
+
+    out.into()
+}
+
+/// Multiply Input By Another Input
+///
+/// @param x A real vector.
+/// @param y A real to multiply.
+/// @returns A real vector with values multiplied by `y`.
+/// @export
+#[savvy]
+fn times_any_numeric(x: RealSexp, y: f64) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedRealSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        if e.is_na() {
+            out[i] = f64::na();
+        } else {
+            out[i] = e * y;
+        }
+    }
+
+    out.into()
+}
+
+/// Flip Input
+///
+/// @param x A logical vector.
+/// @returns A logical vector with filled values (`NA` is converted to `TRUE`).
+/// @export
+#[savvy]
+fn flip_logical(x: LogicalSexp) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedLogicalSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        out.set_elt(i, !e)?;
+    }
+
+    out.into()
+}
+
+/// Or operation
+///
+/// @param x A logical vector.
+/// @param y A logical value.
+/// @returns A logical vector with filled values (`NA` is converted to `TRUE`).
+/// @export
+#[savvy]
+fn or_logical(x: LogicalSexp, y: bool) -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedLogicalSexp::new(x.len())?;
+
+    for (i, e) in x.iter().enumerate() {
+        out.set_elt(i, e || y)?;
+    }
+
+    out.into()
+}
+
+/// Print the content of list
+///
+/// @param x A list vector.
+/// @returns `NULL`
+/// @export
+#[savvy]
+fn print_list(x: ListSexp) -> savvy::Result<()> {
+    for (k, v) in x.iter() {
+        let content = match v.into_typed() {
+            TypedSexp::Integer(x) => {
+                format!(
+                    "integer [{}]",
+                    x.iter()
+                        .map(|i| i.to_string())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                )
+            }
+            TypedSexp::Real(x) => {
+                format!(
+                    "numeric [{}]",
+                    x.iter()
+                        .map(|r| r.to_string())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                )
+            }
+            TypedSexp::String(x) => {
+                format!("character [{}]", x.iter().collect::<Vec<&str>>().join(", "))
+            }
+            TypedSexp::Logical(x) => {
+                format!(
+                    "logical [{}]",
+                    x.iter()
+                        .map(|l| if l { "TRUE" } else { "FALSE" })
+                        .collect::<Vec<&str>>()
+                        .join(", ")
+                )
+            }
+            TypedSexp::List(_) => "list".to_string(),
+            TypedSexp::Null(_) => "NULL".to_string(),
+            TypedSexp::ExternalPointer(_) => "external pointer".to_string(),
+            TypedSexp::Other(_) => "Unsupported".to_string(),
+        };
+
+        let name = if k.is_empty() { "(no name)" } else { k };
+
+        r_print!("{name}: {content}\n");
+    }
+
+    Ok(())
+}
+
+#[savvy]
+fn list_with_no_values() -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedListSexp::new(2, true)?;
+
+    out.set_name(0, "foo")?;
+    out.set_name(1, "bar")?;
+
+    out.into()
+}
+
+#[savvy]
+fn list_with_no_names() -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedListSexp::new(2, false)?;
+
+    let mut e1 = OwnedIntegerSexp::new(1)?;
+    e1[0] = 100;
+
+    let mut e2 = OwnedStringSexp::new(1)?;
+    e2.set_elt(0, "cool")?;
+
+    out.set_value(0, e1)?;
+    out.set_value(1, e2)?;
+
+    out.into()
+}
+
+#[savvy]
+fn list_with_names_and_values() -> savvy::Result<savvy::Sexp> {
+    let mut out = OwnedListSexp::new(2, true)?;
+
+    let mut e1 = OwnedIntegerSexp::new(1)?;
+    e1[0] = 100;
+
+    let mut e2 = OwnedStringSexp::new(1)?;
+    e2.set_elt(0, "cool")?;
+
+    out.set_name_and_value(0, "foo", e1)?;
+    out.set_name_and_value(1, "bar", e2)?;
+
+    out.into()
+}
+
+struct Person {
+    pub name: String,
+}
+
+/// A person with a name
+///
+/// @export
+#[savvy]
+impl Person {
+    fn new() -> Self {
+        Self {
+            name: "".to_string(),
+        }
+    }
+
+    fn set_name(&mut self, name: &str) -> savvy::Result<()> {
+        self.name = name.to_string();
+        Ok(())
+    }
+
+    fn name(&self) -> savvy::Result<savvy::Sexp> {
+        let name = self.name.as_str();
+        name.try_into()
+    }
+
+    fn associated_function() -> savvy::Result<savvy::Sexp> {
+        "associated_function".try_into()
+    }
 }
