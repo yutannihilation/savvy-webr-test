fa36fd8930d675d5378d0f38349e9810d43bc65e
Hiroaki Yutani <yutani.ini@gmail.com>
2024-01-25 08:25:28 +0900

    Remove everything

--- tests/testthat/test-error_handling.R
+++
@@ -1,6 +0,0 @@
-test_that("error handling works", {
-  # check if a Rust objects are dropped properly
-  expect_snapshot(safe_stop(), error = TRUE)
-
-  expect_error(raise_error(), "This is my custom error")
-})
