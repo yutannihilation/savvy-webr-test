6d45e7fd2d5060224461c00b327941496fc41cf7
Hiroaki Yutani <yutani.ini@gmail.com>
2024-01-27 15:26:51 +0900

    Update files

--- src/rust/src/function.rs
+++ src/rust/src/function.rs
@@ -1,25 +1,24 @@
-use savvy::{
-    savvy, sexp::function::FunctionSexp, EnvironmentSexp, ListSexp, OwnedIntegerSexp, Sexp,
-};
+use savvy::{savvy, FunctionArgs, FunctionSexp, ListSexp};
 
 #[savvy]
-pub fn do_call(
-    fun: FunctionSexp,
-    args: ListSexp,
-    env: EnvironmentSexp,
-) -> savvy::Result<savvy::Sexp> {
-    let res = fun.call(args.iter(), &env)?;
+pub fn do_call(fun: FunctionSexp, args: ListSexp) -> savvy::Result<savvy::Sexp> {
+    let args = FunctionArgs::from_list(args)?;
+    let res = fun.call(args)?;
     res.into()
 }
 
 #[savvy]
-pub fn call_with_args(fun: FunctionSexp, env: EnvironmentSexp) -> savvy::Result<savvy::Sexp> {
-    let args = [
-        ("a", Sexp::try_from(1)?),
-        ("b", Sexp::try_from(2.0)?),
-        ("c", Sexp::try_from("foo")?),
-    ]
-    .into_iter();
-    let res = fun.call(args, &env)?;
+pub fn call_with_args(fun: FunctionSexp) -> savvy::Result<savvy::Sexp> {
+    let mut args = FunctionArgs::new();
+    args.add("a", 1)?;
+    args.add("b", 2.0)?;
+    args.add("c", "foo")?;
+    let res = fun.call(args)?;
     res.into()
+}
+
+#[savvy]
+pub fn get_args(args: ListSexp) -> savvy::Result<savvy::Sexp> {
+    let args = FunctionArgs::from_list(args)?;
+    Ok(savvy::Sexp(args.inner()))
 }
